ef4f04524891a482529ca72b5eeedddb1c9380aa
timotewb <timote.wb@gmail.com>
2024-11-23 21:05:38 +1300

    var name fix

--- app/greeting.go
+++ app/greeting.go
@@ -11,13 +11,13 @@ func Greeting() (string){
 
 	greeting := `<b>Welcome to the terminal interface!</b>
 
-    Use commands to navigate and interact with the website.
-    To get started, try entering the command <span class='hst-command'>help</span> and hitting enter.
+Use commands to navigate and interact with the website.
+To get started, try entering the command <span class='hst-command'>help</span> and hitting enter.
 
-    Session start time: {{formattedTime}}
+Session start time: {{formattedTime}}
     `
     result := fmt.Sprintf(greeting, map[string]interface{}{
-        "time": formattedTime,
+        "formattedTime": formattedTime,
     })
 
 	return result
