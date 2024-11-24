9ffc66eb0ddc439068d0993bfc3f496ce6c7b1fd
timotewb <timote.wb@gmail.com>
2024-11-24 20:25:37 +1300

    remove old default

--- main.go
+++ main.go
@@ -6,22 +6,11 @@ import (
 	"github.com/open-runtimes/types-for-go/v4/openruntimes"
 )
 
-type Response struct {
-	Message       string `json:"message"`
-	Ask       string `json:"ask"`
-}
-
-// This Appwrite function will be executed every time your function is triggered
 func Main(Context openruntimes.Context) openruntimes.Response {
-
 	switch Context.Req.Path {
 	case "/greeting": return Context.Res.Text(app.Greeting())
 	case "/home": return Context.Res.Text(app.Home())
 	case "/help": return Context.Res.Text(app.Help())
 	default: return Context.Res.Text(app.Fail())
 	}
-	return Context.Res.Json(Response{
-		Message:   "Thank you for hitting my API",
-		Ask:       "Please done attack me :)",
-	})
 }
