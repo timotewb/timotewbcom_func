8bfb2f34f38fc75f70f7104f1cdc51767789b449
timotewb <timote.wb@gmail.com>
2024-11-24 20:26:43 +1300

    fail update

--- app/fail.go
+++ app/fail.go
@@ -6,7 +6,7 @@ import (
 
 func Fail() (string){
 
-	multiline_string  := `<span class='hst-error'>Error:</span> the <span class='hst-command'>help</span> command does not implement any flags.`
+	multiline_string  := `<span class='hst-error'>Error:</span> the <span class='hst-command'>help</span> command not recognised.`
 
 	multiline_string = strings.ReplaceAll(multiline_string , "\n", "<br>")
 
